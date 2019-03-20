11cbfae884406a6ca9ec077fdd4dcac0452bc04f
seika <43219386+syfgkjasdkn@users.noreply.github.com>
2019-03-20 11:08:00 +0000

    add wallaby

---
+++ test/support/integration_case.ex
@@ -0,0 +1,27 @@
+defmodule SemaWeb.IntegrationCase do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      use Wallaby.DSL
+
+      alias Sema.Repo
+      import Ecto
+      import Ecto.{Changeset, Query}
+
+      alias SemaWeb.Router.Helpers, as: Routes
+    end
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Sema.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(Sema.Repo, {:shared, self()})
+    end
+
+    metadata = Phoenix.Ecto.SQL.Sandbox.metadata_for(Sema.Repo, self())
+    {:ok, session} = Wallaby.start_session(metadata: metadata)
+    {:ok, session: session}
+  end
+end
